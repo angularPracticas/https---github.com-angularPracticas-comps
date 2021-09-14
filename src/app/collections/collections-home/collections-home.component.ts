7d713ec6e10e832a207ddf979664ea324d9fae54
angularPracticas <hugoloyolamaya@gmail.com>
2021-09-14 14:28:01 -0500

    Se finaliza la seccion de collections

--- src/app/collections/collections-home/collections-home.component.ts
+++ src/app/collections/collections-home/collections-home.component.ts
@@ -7,6 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CollectionsHomeComponent implements OnInit {
 
+  headers = [
+    { key: 'employed', label: 'Has a Job'},
+    { key: 'name', label: 'Name'},
+    { key: 'age', label: 'Age' },
+    { key: 'job', label: 'Job' }
+  ];
+
+  data = [
+    { name: 'James', age: 24, job: 'Designer', employed: true },
+    { name: 'Jill', age: 26, job: 'Engineer', employed: false },
+    { name: 'Elyse', age: 25, job: 'Designer', employed: true }
+  ];
+
+  fruits: string[] = ['Apple', 'Orange', 'Banana'];
+
   constructor() { }
 
   ngOnInit(): void {
